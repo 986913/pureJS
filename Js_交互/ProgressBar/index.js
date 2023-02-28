a352f8c13b8a6884144f3cf3c28281df6881a131
Sun flower <MLIU2@LTU.EDU>
2023-02-28 09:12:00 -0500

    progressbar

--- Js_交互/ProgressBar/index.js
+++ Js_交互/ProgressBar/index.js
@@ -1,1 +1,48 @@
-window.onload = function () {};
+(() => {
+  function progressBar($rootElement, initialValue) {
+    const MIN = 0;
+    const MAX = 100;
+
+    const $innerBarEle = document.createElement('div');
+    $innerBarEle.className = 'inner';
+    // Set attributes for a11y.
+    $innerBarEle.setAttribute('role', 'progressbar');
+    $innerBarEle.setAttribute('aria-valuemin', MIN);
+    $innerBarEle.setAttribute('aria-valuemax', MAX);
+
+    function setValue(value) {
+      // Handle invalid values and convert them to be within [0, 100].
+      const clampedValue = Math.min(Math.max(value, MIN), MAX);
+      $innerBarEle.style.width = `${clampedValue}%`;
+      $innerBarEle.textContent = `${clampedValue}%`;
+      $innerBarEle.setAttribute('aria-valuenow', clampedValue);
+    }
+
+    $rootElement.appendChild($innerBarEle);
+    setValue(initialValue);
+
+    // for slider use below
+    return {
+      setValue,
+    };
+  }
+
+  // Initialize some examples.
+  progressBar(document.querySelector('#progress-0'), 0);
+  progressBar(document.querySelector('#progress-2'), 2);
+  progressBar(document.querySelector('#progress-50'), 50);
+  progressBar(document.querySelector('#progress-100'), 100);
+  progressBar(document.querySelector('#progress--10'), -10);
+  progressBar(document.querySelector('#progress-120'), 120);
+
+  // Initialize the slider example which controls the progress of the progress bar below.
+  const progressSlider = progressBar(
+    document.querySelector('#progress-slider'),
+    50
+  );
+
+  document.querySelector('#slider').addEventListener('change', (event) => {
+    console.log(event.target);
+    progressSlider.setValue(event.target.value);
+  });
+})();
