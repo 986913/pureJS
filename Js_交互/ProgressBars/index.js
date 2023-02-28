88b9fa23fd8537a5496b4e5b543791a082e06f6a
Sun flower <MLIU2@LTU.EDU>
2023-02-28 16:39:16 -0500

    progress bars

---
+++ Js_交互/ProgressBars/index.js
@@ -0,0 +1,29 @@
+(() => {
+  const createProgressBar = () => {
+    const outter = document.createElement('div');
+    const inner = document.createElement('div');
+    outter.classList.add('outter');
+    inner.classList.add('inner');
+    outter.appendChild(inner);
+    wrapper.appendChild(outter);
+
+    fillBar(2, inner);
+  };
+
+  const fillBar = (seconds, bar) => {
+    const frequency = (seconds * 1000) / 100; //calculate frequency to update width
+    let atPercent = 0;
+
+    // set a interval timer here
+    const interval = setInterval(() => {
+      bar.style.width = atPercent + '%';
+      atPercent++;
+
+      if (atPercent > 100) clearInterval(interval); // stop interval if percent is > 100
+    }, frequency);
+  };
+
+  const wrapper = document.querySelector('.wrapper');
+  const btn = document.getElementById('add');
+  btn.addEventListener('click', createProgressBar);
+})();
