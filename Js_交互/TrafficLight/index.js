313dda304571cb25c9c7637c7e71711db6bcd200
Sun flower <MLIU2@LTU.EDU>
2023-03-17 13:15:11 -0400

    purejs

---
+++ Js_交互/TrafficLight/index.js
@@ -0,0 +1,76 @@
+const config = {
+  red: {
+    duration: 4000,
+    next: 'green',
+  },
+  yellow: {
+    duration: 500,
+    next: 'red',
+  },
+  green: {
+    duration: 3000,
+    next: 'yellow',
+  },
+};
+
+(() => {
+  const light = ({ backgroundColor }) => {
+    const $lightEl = document.createElement('div');
+    $lightEl.classList.add('light');
+    $lightEl.setAttribute('aria-hidden', true);
+
+    if (backgroundColor != null) {
+      $lightEl.style.backgroundColor = backgroundColor;
+    }
+
+    return $lightEl;
+  };
+
+  const trafficLight = ($rootEl, { initialColor, config }) => {
+    let currentColor = initialColor;
+    let timer = null;
+
+    const $containerEl = document.createElement('div');
+    $containerEl.classList.add('lights-wrapper');
+    $containerEl.setAttribute('aria-live', 'polite');
+
+    const setTransition = () => {
+      const { duration, next } = config[currentColor];
+      timer = setTimeout(() => {
+        currentColor = next;
+        renderLoop();
+      }, duration);
+    };
+
+    const render = () => {
+      $containerEl.innerHTML = '';
+      $containerEl.setAttribute('aria-label', `Current light: ${currentColor}`);
+
+      Object.keys(config).map((color) => {
+        $containerEl.append(
+          light({
+            backgroundColor: color === currentColor ? currentColor : undefined,
+          })
+        );
+      });
+    };
+
+    const renderLoop = () => {
+      render();
+      setTransition();
+    };
+
+    // The beforeunload event is fired before the tab/window is closed. Clear the timer when the tab/window is about to be closed.
+    window.addEventListener('beforeunload', () => {
+      window.clearInterval(timer);
+    });
+
+    $rootEl.append($containerEl);
+    renderLoop();
+  };
+
+  trafficLight(document.getElementById('light1'), {
+    initialColor: 'red',
+    config,
+  });
+})();
