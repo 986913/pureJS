05aeb9f4286c16d4cce975abfb240fd0633ead53
Sun flower <MLIU2@LTU.EDU>
2023-03-19 15:18:12 -0400

    text search I

---
+++ Js_交互/TemperatureConverter/index.js
@@ -0,0 +1,30 @@
+(() => {
+  const $celsiusInputEl = document.getElementById('celsius-input');
+  const $fahrenheitInputEl = document.getElementById('fahrenheit-input');
+
+  function bindInput($source, $dest, calculateValueCallBack) {
+    $source.addEventListener('input', (event) => {
+      const numericValue = Number(event.target.value);
+      const isValid =
+        !Number.isNaN(numericValue) && Boolean(event.target.value);
+      $dest.value = isValid ? format(calculateValueCallBack(numericValue)) : '';
+    });
+  }
+
+  function format(number) {
+    // Show 4 d.p. if number has more than 4 decimal places.
+    return /\.\d{5}/.test(number) ? Number(number).toFixed(4) : number;
+  }
+
+  bindInput(
+    $celsiusInputEl,
+    $fahrenheitInputEl,
+    (value) => (value * 9) / 5 + 32
+  );
+
+  bindInput(
+    $fahrenheitInputEl,
+    $celsiusInputEl,
+    (value) => ((value - 32) * 5) / 9
+  );
+})();
---
+++ Js_交互/TextSearch/index.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} string
+ * @param {string} query
+ * @return {string}
+ */
+
+function textSearch(setence, query) {
+  if (!setence || !query) return setence; // edge case
+
+  let result = '';
+  const boldChars = Array(setence.length).fill(false);
+
+  // 1. identification of bold characters
+  for (let i = 0; i < setence.length; ) {
+    const substr = setence.slice(i, i + query.length);
+    if (substr.toLowerCase() === query.toLowerCase()) {
+      //fill with true with same length as query's (from position i until position i+query.length)
+      boldChars.fill(true, i, i + query.length);
+      i += query.length; //if found, need jump i to query.length as unit
+    } else {
+      i++;
+    }
+  }
+  // console.log(boldChars);
+
+  // 2. rendering of the <b> tags
+  for (let i = 0; i < setence.length; i++) {
+    const shouldOpenTag = boldChars[i] && !boldChars[i - 1];
+    const shouldCloseTag = boldChars[i] && !boldChars[i + 1];
+
+    let char = setence[i];
+    if (shouldOpenTag) char = `<b>${char}`;
+    if (shouldCloseTag) char = `${char}</b>`;
+
+    result += char;
+  }
+
+  return result;
+}
+
+/**-- test above function-------------------------- */
+
+console.log(textSearch('The Quick Brown Fox Jumps Over The Lazy Dog', 'fox')); // 'The Quick Brown <b>Fox</b> Jumps Over The Lazy Dog'
+console.log(textSearch('The hardworking Dog overtakes the lazy dog', 'dog')); // 'The hardworking <b>Dog</b> overtakes the lazy <b>dog</b>'
+console.log(textSearch('aaa', 'aa')); // '<b>aa</b>a'  This is because the second character cannot be used as a match
+console.log(textSearch('aaaa', 'aa')); // Correct: '<b>aaaa</b>'  // Wrong: '<b>aa</b><b>aa</b>'
