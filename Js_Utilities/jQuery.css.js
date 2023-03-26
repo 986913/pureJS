09708927b1b909665725ff2f8cd662d9ce0473cf
Sun flower <MLIU2@LTU.EDU>
2023-03-26 15:43:57 -0400

    2nd solution

--- Js_Utilities/jQuery.css.js
+++ Js_Utilities/jQuery.css.js
@@ -10,7 +10,7 @@ $('button')
   .css('backgroundColor', 'tomato')
   .css('fontSize', '16px');
 
-/* -------------------------- Code Solution: -------------------------------- */
+/* -------------------------- Code Solution: (use closure) -------------------------------- */
 /**
  * @param {string} selector
  * @return {{css: Function}}
@@ -35,4 +35,36 @@ export default function $(selector) {
       return this; //to allow for method chaining
     },
   };
+}
+
+
+/* -------------------------- alternative Code Solution (use class) -------------------------------- */
+class jQuery {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+  }
+
+  css(prop, value) {
+    // Getter case.
+    if (value === undefined) {
+      // No matching elements.
+      if (this.element == null) {
+        return undefined;
+      }
+
+      const value = this.element.style[prop];
+      return value === '' ? undefined : value;
+    }
+
+    // Setter case.
+    if (this.element != null) {
+      this.element.style[prop] = value;
+    }
+
+    return this;
+  }
+}
+
+export default function $(element) {
+  return new jQuery(element);
 }
