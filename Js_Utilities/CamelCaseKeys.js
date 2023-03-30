880e513347271476f5e9f188a7bada85a31ecc26
Sun flower <MLIU2@LTU.EDU>
2023-03-30 12:33:07 -0400

    deepclone and bianxing

--- Js_Utilities/CamelCaseKeys.js
+++ Js_Utilities/CamelCaseKeys.js
@@ -5,10 +5,41 @@
 
 /* --------------------- 用例测试: 实现这种函数 ----------------------- */
 camelCaseKeys({ foo_bar: true }); // { fooBar: true }
-
 camelCaseKeys({ foo_bar: true, bar_baz: { baz_qux: '1' } }); // { fooBar: true, barBaz: { bazQux: '1' } }
-
 camelCaseKeys([{ baz_qux: true }, { foo: true, bar: [{ foo_bar: 'hello' }] }]);
 // [{ bazQux: true }, { foo: true, bar: [{ fooBar: 'hello' }] }]
 
-/* -------------------------------- Code solution ---------------------------------- */
+/* -------------------------------- Code solution: 🟡 DeepClone变形题 ---------------------------------- */
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function camelCase(str) {
+  if (!/[_]/.test(str)) return str;
+
+  return str
+    .toLowerCase()
+    .replace(/([_])([a-z])/g, (_match, _p1, p2) => p2.toUpperCase());
+}
+
+/**
+ * @param {Object} value
+ * @return {Object}
+ */
+function camelCaseKeys(value) {
+  // when value is primitive OR value is null:
+  if (typeof value !== 'object' || value === null) return value;
+
+  // when value is array:
+  if (Array.isArray(value)) {
+    return value.map((item) => camelCaseKeys(item));
+  }
+
+  // when value is object:
+  const entries = Object.entries(value);
+  const keyConverted_Cloned_Entries = entries.map(([key, value]) => [
+    camelCase(key),
+    camelCaseKeys(value),
+  ]);
+  return Object.fromEntries(keyConverted_Cloned_Entries);
+}
---
+++ Js_Utilities/DeepClone.js
@@ -0,0 +1,61 @@
+/* deep clone: 
+    可以对 JavaScript 中的5种主要的数据类型（包括Number、String、Object、Array、Boolean）进行clone 
+*/
+
+/* --------------------- 用例测试: 实现这种函数 ----------------------- */
+const obj1 = { user: { role: 'admin' } };
+const clonedObj1 = deepClone(obj1);
+
+clonedObj1.user.role = 'guest'; // Change the cloned user's role to 'guest'.
+clonedObj1.user.role; // 'guest'
+obj1.user.role; // Should still be 'admin'.
+
+const obj2 = { foo: [{ bar: 'baz' }] };
+const clonedObj2 = deepClone(obj2);
+
+obj2.foo[0].bar = 'bax'; // Modify the original object.
+obj2.foo[0].bar; // 'bax'
+clonedObj2.foo[0].bar; // Should still be 'baz'.
+
+/* ------------- Code solution 1 : using for...in + recurrsion ---------------------------------- */
+function deepClone(value) {
+  //递归终止时候：
+  if (typeof value !== 'object' || value === null) return value;
+
+  //单层递归逻辑：
+  const isArr = Array.isArray(value);
+  let result = isArr ? [] : {};
+  for (let key in value) {
+    // 保证key不是原型上的属性
+    if (value.hasOwnProperty(key)) {
+      // recurrsion:
+      result[key] = deepClone(value[key]);
+    }
+  }
+
+  return result;
+}
+
+/* ------------- Code solution 2 : using Object.fromEntries + recurrsion ---------------------------------- */
+function deepClone(value) {
+  //when value is primitive:
+  if (typeof value !== 'object' || value === null) return value;
+
+  // when value is array: recurrsion
+  if (Array.isArray(value)) {
+    return value.map((item) => deepClone(item));
+  }
+
+  //when value is object, Converting the object into an array of a key-value tuple with Object.entries, and starting recursion
+  const entries = Object.entries(value);
+  const clonedEntries = entries.map(([key, value]) => [key, deepClone(value)]);
+  return Object.freomEntries(clonedEntries);
+}
+
+/***
+ * 知识点：
+ * 1. 使用 for … in 进行遍历object时候，它也会遍历原型上的属性。所以在一些case处理上，要用.hasOwnProperty(key)判断key是不是原型上的属性.
+ * 2. Object.entries( {foo:'bar', baz:42} )             --->  [ ['foo', 'bar'], ['baz', 42] ]
+ * 3. Object.fromEntries ( [['foo','bar'],['baz',42]] ) --->  {foo:'bar', baz:42}
+ *
+ */
