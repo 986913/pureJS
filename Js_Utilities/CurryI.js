0a70d10f78db5c65936ec9405ead655d2c17d18b
Sun flower <MLIU2@LTU.EDU>
2023-04-01 11:01:22 -0400

    commit edit

--- Js_Utilities/CurryI.js
+++ Js_Utilities/CurryI.js
@@ -1,9 +1,16 @@
+/**
+ * Currying 定义：
+ * Currying is the technique of converting a function that takes multiple arguments into
+ * a sequence of functions that each takes a single argument.
+ */
+
 /* --------------------- 用例测试1 ----------------------- */
 const sum = (a, b) => a + b;
 
 const curriedSum = curry(sum);
-curriedSum(3)(4); // 7
-curriedSum(3, 4); // 7
+curriedSum(3)(4); // 7 or curry(sum)(3)(4)
+curriedSum(3, 4); // 7 or curry(sum)(3, 4)
+
 // const alreadyAddedThree = curriedSum(3);
 // alreadyAddedThree(4); // 7
 
@@ -11,10 +18,10 @@ curriedSum(3, 4); // 7
 const multiplyThree = (a, b, c) => a * b * c;
 
 const curriedMultiplyThree = curry(multiplyThree);
-curriedMultiplyThree(4)(5)(6); // 120
-curriedMultiplyThree(4, 5, 6); // 120
-curriedMultiplyThree(4, 5), 6; // 120
-curriedMultiplyThree(4), (5, 6); // 120
+curriedMultiplyThree(4)(5)(6); // 120  or curry(multiplyThree)(4)(5)(6)
+curriedMultiplyThree(4, 5, 6); // 120  or curry(multiplyThree)(4, 5, 6)
+curriedMultiplyThree(4, 5), 6; // 120  or curry(multiplyThree)(4, 5)(6)
+curriedMultiplyThree(4), (5, 6); // 120or curry(multiplyThree)(4)(5, 6)
 // const containsFour = curriedMultiplyThree(4);
 // const containsFourMulFive = containsFour(5);
 // containsFourMulFive(6); // 120
@@ -23,9 +30,9 @@ curriedMultiplyThree(4), (5, 6); // 120
 const join = (a, b, c) => `${a}_${b}_${c}`;
 
 const curriedJoin = curry(join);
-curriedJoin(1, 2, 3); // '1_2_3'
-curriedJoin(1)(2, 3); // '1_2_3'
-curriedJoin(1, 2)(3); // '1_2_3'
+curriedJoin(1, 2, 3); // '1_2_3' or curry(join)(1)(2)(3)
+curriedJoin(1)(2, 3); // '1_2_3' or curry(join)(1)(2, 3)
+curriedJoin(1, 2)(3); // '1_2_3' or curry(join)(1, 2)(3)
 
 /* -------------------------------- Code solution ---------------------------------- */
 /**
---
+++ Js_Utilities/CurryII.js
@@ -0,0 +1,9 @@
+/**
+ * Currying 定义：
+ * Currying is the technique of converting a function that takes multiple arguments into
+ * a sequence of functions that each takes a single argument.
+ */
+
+/* --------------------- 用例测试1 ----------------------- */
+
+/* -------------------------------- Code solution ---------------------------------- */
