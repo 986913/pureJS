1b8b12e39200d7524552756dd95fff37352a2147
Sun flower <MLIU2@LTU.EDU>
2023-05-09 19:48:47 -0400

    is same dom tree

--- Js_Utilities/IsSameDOMTrees.js
+++ Js_Utilities/IsSameDOMTrees.js
@@ -17,8 +17,8 @@
       <div id="foo">Hello World</div>
  */
 
-/* -------------------------- Code Solution - lc 100(is same tree)-👍 DFS post_order recursion-------------------------------- */
-
+/* -------------------------- Code Solution - lc 100(is same tree变形题)-👍 DFS post_order recursion-------------------------------- */
+/*
 // 1. recurison params: 2 tree node:p and q, return boolean
 var isSameTree = function (p, q) {
   //2. make sure when recursion ends:
@@ -30,6 +30,36 @@ var isSameTree = function (p, q) {
   //3. recursion single layer logic (when p.val===q.val)
   let isLeftSideSame = isSameTree(p.left, q.left);
   let isRightSideSame = isSameTree(p.right, q.right);
-
   return isLeftSideSame && isRightSideSame;
 };
+*/
+
+/**
+ * @param {Node} nodeA
+ * @param {Node} nodeB
+ * @return {boolean}
+ */
+
+//step 1.  recursion params: two treenode,  return boolean
+function isSameTree(nodeA, nodeB) {
+  /* step2: make sure when recurrsion ends, below are cases: */
+  if (nodeA.nodeType !== nodeB.nodeType) return false;
+  if (nodeA.nodeType === Node.TEXT_NODE) {
+    return nodeA.textContent === nodeB.textContent;
+  }
+  if (nodeA.childNodes.length !== nodeB.childNodes.length) return false;
+  if (nodeA.attributes.length !== nodeB.attributes.length) return false;
+  const hasSameAttributes = nodeA
+    .getAttributeNames()
+    .every(
+      (attrName) =>
+        nodeA.getAttribute(attrName) === nodeB.getAttribute(attrName)
+    );
+  if (!hasSameAttributes) return false;
+
+  /* step3: start recursion single layer logic:
+    because we don't know how may of the current node's children amount.  so we can iterate childrens instead of node.left,node right(leetcode100), and pass in childA and nodeB.childNodes[index] to recurrsion 👒*/
+  return Array.from(nodeA.childNodes).every((childA, index) => {
+    return isSameTree(childA, nodeB.childNodes[index]);
+  });
+}
