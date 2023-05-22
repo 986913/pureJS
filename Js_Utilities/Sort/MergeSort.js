43f3e55e66989986a2e97e2ef33b548e324bf7eb
Sun flower <MLIU2@LTU.EDU>
2023-05-22 13:24:44 -0400

    add new sol

--- Js_Utilities/Sort/MergeSort.js
+++ Js_Utilities/Sort/MergeSort.js
@@ -51,6 +51,43 @@ function merge(arr1, arr2) {
 }
 
 /* ---------------------------------------- Solution: (In-place) ------------------------------------------------------ */
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  let res = merge(left, right);
+  arr.forEach((_, index) => (arr[index] = res[index]));
+
+  return arr;
+}
+// helper function: merges two sorted arrays (use 2 pointer)
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
 
 /*********************  变形题🟡 -> https://bigfrontend.dev/problem/merge-sorted-arrays ******************/
 function merge2Lists(arr1, arr2) {
