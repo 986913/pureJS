b54e0971644e9c618ed94252683e84422c610562
Sun flower <MLIU2@LTU.EDU>
2023-05-27 16:51:27 -0400

    comemnts change

--- Js_Utilities/TextSearchII.js
+++ Js_Utilities/TextSearchII.js
@@ -1,5 +1,5 @@
 /*------------------- 用例测试 -------------------- */
-textSearch('The Quick Brown Fox Jumps Over The Lazy Dog', ['fox']);
+textSearch('The Quick Brown Fox Jumps Over The Lazy Dog', ['fox', '  ']);
 // 'The Quick Brown <b>Fox</b> Jumps Over The Lazy Dog'
 
 textSearch('The quick brown fox jumps over the lazy dog', ['fox', 'dog']);
@@ -20,22 +20,22 @@ textSearch('aaaa', ['aa']);
 
 /* ------------------ Solution Code ---------------------------------------------------- */
 /**
- * @param {string} string
+ * @param {string} setence
  * @param {Array<string>} queries
  * @return {string}
  */
 
-function textSearch(string, queries) {
-  if (string.trim() === '') return string;
+function textSearch(setence, queries) {
+  if (setence.trim() === '') return setence;
 
-  const boldChars = Array(string.length).fill(false);
+  const boldChars = Array(setence.length).fill(false);
 
-  // 1. identification of bold characters
+  // 1. identification of bold characters: 外循环quries, 内循环setence
   for (const query of queries) {
-    if (query.trim() === '') continue;
+    if (query.trim() === '') continue; // edge case: '  '
 
-    for (let i = 0; i < string.length; ) {
-      const substr = string.slice(i, i + query.length);
+    for (let i = 0; i < setence.length; ) {
+      const substr = setence.slice(i, i + query.length);
       if (substr.toLowerCase() === query.toLowerCase()) {
         boldChars.fill(true, i, i + query.length);
         // Start from next character if there's a match since one character cannot match the same query more than once.
@@ -48,19 +48,17 @@ function textSearch(string, queries) {
   // console.log(boldChars);
 
   // 2. rendering of the <b> tags
-  let highlightedString = '';
-  for (let i = 0; i < string.length; i++) {
-    // When the current character should be bolded and the previous character should not be bolded, append an opening tag to the final string.
+  let result = '';
+  for (let i = 0; i < setence.length; i++) {.
     const shouldAddOpeningTag = boldChars[i] && !boldChars[i - 1];
-    // When the current character should be bolded and the next character should not be bolded, append a closing tag to the final string.
     const shouldAddClosingTag = boldChars[i] && !boldChars[i + 1];
 
-    let char = string[i];
+    let char = setence[i];
     if (shouldAddOpeningTag) char = `<b>${char}`;
     if (shouldAddClosingTag) char = `${char}</b>`;
 
-    highlightedString += char;
+    result += char;
   }
 
-  return highlightedString;
+  return result;
 }
