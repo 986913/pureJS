894b8b687a9ff747a743d0bb6e571b077eb52615
Sun flower <MLIU2@LTU.EDU>
2023-05-30 18:32:41 -0400

    add new

---
+++ Js_Utilities/addComma.js
@@ -0,0 +1,10 @@
+/**
+  Given a number, please create a function to add commas as thousand separators. Input are all valid numbers.
+ */
+/* --------------------- 用例测试 ----------------------- */
+addComma(1); // '1'
+addComma(1000); // '1,000'
+addComma(-12345678); // '-12,345,678'
+addComma(12345678.12345); // '12,345,678.12345'
+
+/* ------------------ Code solution ------------------ */
