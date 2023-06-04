31588d034995bde4a2517049986264f0ce101e05
Sun flower <MLIU2@LTU.EDU>
2023-06-04 18:51:42 -0400

    modify deep euqal

--- Js_Utilities/DeepEqual.js
+++ Js_Utilities/DeepEqual.js
@@ -15,6 +15,19 @@ const v7 = [{ id: '1' }];
 const v8 = [{ id: '2' }];
 deepEqual(v7, v8); // false
 
+const a = [1, 2];
+a[2] = a;
+const b = [1, 2];
+b[2] = b;
+const c = [1, 2, [1, 2, a]];
+const d = [1, 2, [1, 2, b]];
+deepEqual(a, b); // true
+deepEqual(a, c); // true
+deepEqual(a, d); // true
+deepEqual(b, c); // true
+deepEqual(b, d); // true
+deepEqual(c, d); // true
+
 /* -------------------------------- Code solution  -------------------------------------- */
 /**
  * @param {*} valueA
@@ -23,26 +36,30 @@ deepEqual(v7, v8); // false
  */
 
 const getType = (val) => Object.prototype.toString.call(val);
-
 const shouldDeepCompare = (type) =>
   type === '[object Object]' || type === '[object Array]';
 
-function deepEqual(valueA, valueB) {
-  const typeA = getType(valueA);
-  const typeB = getType(valueB);
+function deepEqual(a, b, cache = new Map()) {
+  const typeA = getType(a);
+  const typeB = getType(b);
+
+  // Check for circular reference
+  if (cache.has(a) || cache.has(b)) return true;
+  cache.set(a, true);
+  cache.set(b, true);
 
   // 当valueA,valueB同时是array 或者 当valueA,valueB同时是object时： we traverse into them by calling `isEqual` again.
-  if (typeA === typeB && shouldDeepCompare(typeA) && shouldDeepCompare(typeB)) {
-    const kvPairsA = Object.entries(valueA);
-    const kvPairsB = Object.entries(valueB);
+  if (typeA === typeB && shouldDeepCompare(typeA)) {
+    const kvPairsA = Object.entries(a);
+    const kvPairsB = Object.entries(b);
 
     if (kvPairsA.length !== kvPairsB.length) return false;
 
     // Make sure the other objects have the same properties defined.
-    return kvPairsA.every(
-      ([key, val]) => Object.hasOwn(valueB, key) && deepEqual(val, valueB[key])
-    );
+    return kvPairsA.every(([key, value]) => {
+      return b.hasOwnProperty(key) && deepEqual(value, b[key], cache);
+    });
   }
 
-  return Object.is(valueA, valueB);
+  return Object.is(a, b);
 }
