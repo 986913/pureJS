9d558fab81477fe2bcfd95316570ab281450b12c
Sun flower <MLIU2@LTU.EDU>
2023-06-27 19:41:59 -0400

    throttle promise

--- Js_Utilities/Performance_throttlePromises.js
+++ Js_Utilities/Performance_throttlePromises.js
@@ -13,3 +13,79 @@ throttleAsync(callApis, 5)
   .catch((err) => {
     // any error occurs in the callApis would be relayed here
   });
+
+/* ------------------------- Code solution 1 --------------------------- */
+/**
+ * @param {() => Promise<any>} func
+ * @param {number} max
+ * @return {Promise}
+ */
+function throttlePromises(funcs, max) {
+  return new Promise((resolve, reject) => {
+    let concurrentCount = 0;
+    let latestCalledFnIdx = -1;
+    let resultCount = 0;
+    let hasErr = false;
+    const result = [];
+
+    const runNext = () => {
+      // stop call next function if has err OR reach to the end of functions
+      if (hasErr || latestCalledFnIdx === funcs.length - 1) return;
+
+      const nextFnIdx = latestCalledFnIdx + 1; // 下一个被call的fn的index
+      const nextFn = funcs[nextFnIdx]; // 下一个被call的fn
+      concurrentCount += 1;
+      latestCalledFnIdx += 1;
+
+      // actuall call next function
+      nextFn().then(
+        (data) => {
+          result[nextFnIdx] = data; // update result
+          resultCount++;
+          concurrentCount -= 1;
+
+          if (resultCount === funcs.length) {
+            resolve(result); //等所有funcs都call完了， 然后在统一reslove result出去
+            return;
+          }
+
+          runNext();
+        },
+        (err) => {
+          hasErr = true;
+          reject(err);
+        }
+      );
+
+      if (concurrentCount < max) runNext();
+    };
+
+    runNext();
+  });
+}
+/* ------------------------- Code solution 2 --------------------------- */
+function throttlePromises(funcs, max) {
+  let results = [];
+
+  return new Promise((resolve, reject) => {
+    let runningCount = 0;
+    let queue = [...funcs];
+
+    function run() {
+      while (runningCount < max && queue.length > 0) {
+        const fn = queue.shift();
+        runningCount++;
+        fn()
+          .then((data) => {
+            runningCount--;
+            results.push(data);
+            run();
+          })
+          .catch((err) => reject(err));
+      }
+      if (results.length === funcs.length) resolve(results);
+    }
+
+    run();
+  });
+}
