fa1bcacf58b55203128fb348829288db020a7460
Sun flower <MLIU2@LTU.EDU>
2023-07-04 15:26:08 -0400

    add comments

--- Js_Utilities/myCookie.js
+++ Js_Utilities/myCookie.js
@@ -77,3 +77,35 @@ function install() {
 function uninstall() {
   delete document.myCookie;
 }
+
+/**
+  Object.defineProperties():
+    - 作用：用于定义或修改一个对象的1个或多个属性及其对应的属性discriptor
+    - 返回：修改后的对象
+    - 用法：有2️⃣个用法：
+        1. 如果你只想定义/修改1个属性 这时需要提供第一个参数obj，第二个参数prop，和第三个参数 descriptor：
+          const obj2 = {};
+          Object.defineProperty(obj2, 'name', {
+            value: 'John',
+            writable: false,
+            enumerable: true
+          });
+          console.log(obj2); // { name: 'John' }
+        2. 如果你想一次性定义/修改多个属性。 这时要提供第一个参数obj 和一个包含属性discriptors的对象作为第二个参数. 不需要提供第三个参数，因为所有的属性描述符都包含在第二个参数的对象中。
+          const obj = {};
+          Object.defineProperties(obj, {
+            name: {
+              value: 'John',
+              writable: true,
+              enumerable: true,
+              configurable: true
+            },
+            age: {
+              value: 25,
+              writable: false,
+              enumerable: true,
+              configurable: false
+            }
+          });
+          console.log(obj); // { name: 'John', age: 25 }
+ */
