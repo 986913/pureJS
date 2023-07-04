bf660e0d4674430e2c6d0ce006f7012dc9b848f4
Sun flower <MLIU2@LTU.EDU>
2023-07-04 16:03:52 -0400

    add new

---
+++ Js_Utilities/myATob.js
@@ -0,0 +1,12 @@
+/**
+  atob() decodes a string of data which has been encoded using Base64 encoding.
+  
+  Please implement your own myAtob()
+    myAtob('QkZFLmRldg==') // 'BFE.dev'
+    myAtob('Q') // Error
+
+  Please don't use atob() directly in your code.
+ */
+/* -------------------用例测试 --------------------*/
+
+/* -------------------------- Code Solution: -------------------------------- */
