7c8e10a006eb4af3ba29b265584b826b38ce5bc7
Sun flower <MLIU2@LTU.EDU>
2023-07-05 14:27:31 -0400

    add comment

--- Js_Utilities/lit-html_I.js
+++ Js_Utilities/lit-html_I.js
@@ -12,6 +12,31 @@
   The input data are all valid.
  */
 
-/* -------------------用例测试 --------------------*/
-
 /* -------------------------- Code Solution: -------------------------------- */
+const html = (statics, ...dynamicValues) =>
+  statics.map((statics, i) => `${statics}${dynamicValues[i] ?? ''}`).join('');
+
+const render = (result, container) => {
+  container.innerHTML = result;
+};
+
+/*
+  html is an Tag Function here  ：
+    这种参数的设计允许我们在标签函数中处理模板字面量的内容，将静态字符串和表达式的值组合起来，最终生成所需的结果。
+
+  当我们使用string template调用html函数时，模板字面量的内容将被分解成两个参数传递给html函数:
+    第一个参数是一个数组，包含了所有静态字符串片段（即没有表达式的部分)
+    第二个参数是一个不定数量的参数，包含了模板字面量中的表达式的值
+  举个例子：
+    const template = (name, title) => html`i am ${name}, my job is ${title}`
+    template('ming', 'SDE')
+      那么html函数中:
+        segements是数组：["i am", ", my job is ", ""]
+        values是数组：   ["ming", "SDE"]
+*/
+
+/**
+  ?? 是一个空值合并运算符（nullish coalescing operator):
+    如果dynamicValues[i]的值不为null或undefined，则表达式${dynamicValues[i] ?? ''} 将返回dynamicValues[i]的值。
+    如果dynamicValues[i]的值为null或undefined，  则表达式${dynamicValues[i] ?? ''} 将返回空字符串''作为默认值。
+ */
