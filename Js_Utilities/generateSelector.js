41659057e3c57fb4ed7b1907a0157b34d384e6b5
Sun flower <MLIU2@LTU.EDU>
2023-07-23 15:07:46 -0400

    generate selector

--- Js_Utilities/generateSelector.js
+++ Js_Utilities/generateSelector.js
@@ -14,9 +14,29 @@
   There could be more than 1 answer.
     let selector = generateSelector(root, target) // 'button'
     expect(root.querySelector(selector)).toBe(target)
+
     selector = generateSelector(root, target) // 'div > div > p > button'
     expect(root.querySelector(selector)).toBe(target)
 
  */
 
-/* -------------------------- Code Solution: -------------------------------- */
+/* -------------------------- Code Solution 1: while 迭代 -------------------------------- */
+/**
+ * @param {HTMLElement} root
+ * @param {HTMLElement} target
+ * @return {string}
+ */
+
+function generateSelector(root, target) {
+  if (target.id) return `#${target.id}`;
+
+  let currentNode = target;
+  const path = [];
+
+  while (currentNode !== root) {
+    path.unshift(currentNode.tagName.toLocaleLowerCase());
+    currentNode = currentNode.parentElement; // update currentNode to its parent here
+  }
+
+  return path.join('>');
+}
