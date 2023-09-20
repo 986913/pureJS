426c1bfe466b89293c1eaedbc7d0415507fd89a4
Sun flower <MLIU2@LTU.EDU>
2023-09-20 16:48:59 -0400

    add examples

---
+++ Js_DesignPattern/Creational Design Pattern/Builder/eg2.js
@@ -0,0 +1,69 @@
+class Assignment {
+  make(builder) {
+    builder.step1();
+    builder.step2();
+    builder.step3();
+    builder.step4();
+    return builder.get();
+  }
+}
+
+class AssignmentBuilder {
+  constructor(subject, level, dueDate) {
+    this.assignment = null;
+    this.subject = subject;
+    this.level = level;
+    this.dueDate = dueDate;
+  }
+  step1() {
+    this.assignment = new Task();
+  }
+  step2() {
+    this.assignment.addSubject(this.subject);
+  }
+  step3() {
+    this.assignment.addLevel(this.level);
+  }
+  step4() {
+    this.assignment.addDuedate(this.dueDate);
+  }
+  get() {
+    return this.assignment;
+  }
+}
+
+class Task {
+  constructor() {
+    this.subject = null;
+    this.level = null;
+    this.dueDate = null;
+  }
+  addSubject(subject) {
+    this.subject = subject;
+  }
+  addLevel(level) {
+    this.level = level;
+  }
+  addDuedate(dueDate) {
+    this.dueDate = dueDate;
+  }
+  announcement() {
+    console.log(
+      `Your ${this.subject} assignment's difficulty level is: ${this.level}. It is due on ${this.dueDate}.`
+    );
+  }
+}
+
+/** --------------------------- 用例测试 ------------------------------- **/
+try {
+  var assignment = new Assignment();
+  var assignmentBuilder = new AssignmentBuilder(
+    'Math',
+    'Hard',
+    '12th June, 2020'
+  );
+  var mathAssignment = assignment.make(assignmentBuilder);
+  mathAssignment.announcement(); // Your Math assignment's difficulty level is: Hard. It is due on 12th June, 2020.
+} catch (e) {
+  console.log(e);
+}
