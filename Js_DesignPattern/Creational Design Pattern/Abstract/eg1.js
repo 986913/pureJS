4f997fb9702e9803ba17b879d80ec6d0161dc980
Sun flower <MLIU2@LTU.EDU>
2023-09-24 14:19:53 -0400

    add new

--- Js_DesignPattern/Creational Design Pattern/Abstract/eg1.js
+++ Js_DesignPattern/Creational Design Pattern/Abstract/eg1.js
@@ -1,1 +1,48 @@
+function JunkFoodFactory() {
+  var junkfood;
+  this.createJunkFood = function (name, type, price) {
+    switch (name) {
+      case 'chips':
+        junkfood = new Chips(name, type, price);
+        break;
+      case 'soda':
+        junkfood = new Soda(name, type, price);
+        break;
+      default:
+        junkfood = new Chips(name, type, price);
+        break;
+    }
+    return junkfood;
+  };
+}
+
+function Soda(name, type, price) {
+  this.name = name;
+  this.type = type;
+  this.price = price;
+  this.display = function () {
+    console.log(`The ${this.type} ${this.name} costs ${this.price} dollars`);
+  };
+}
+function Chips(name, type, price) {
+  this.name = name;
+  this.type = type;
+  this.price = price;
+  this.display = function () {
+    console.log(`The ${this.type} ${this.name} costs ${this.price} dollars`);
+  };
+}
+
 /** --------------------------- 用例测试 ------------------------------- **/
+var factory = new JunkFoodFactory();
+var chips = factory.createJunkFood('chips', 'potato', 1.5);
+chips.display(); // The potato chips costs 1.5 dollars
+
+chips = factory.createJunkFood('chips', 'corn', 2.5);
+chips.display(); // The corn chips costs 2.5 dollars
+
+var soda = factory.createJunkFood('soda', 'Energy Drink', 10);
+soda.display(); // The Energy Drink soda costs 10 dollars
+
+soda = factory.createJunkFood('soda', 'Cola', 7);
+soda.display(); // The Cola soda costs 7 dollars
---
+++ Js_DesignPattern/Creational Design Pattern/Abstract/eg2.js
@@ -0,0 +1,62 @@
+class Loans {
+  constructor() {
+    this.loans = null;
+  }
+  getloan(type, amount, duration) {
+    switch (type) {
+      case 'home':
+        this.loans = new HomeLoan(amount, duration);
+        break;
+      case 'student':
+        this.loans = new StudentLoan(amount, duration);
+        break;
+      case 'personal':
+        this.loans = new PersonalLoan(amount, duration);
+        break;
+    }
+    return this.loans;
+  }
+}
+
+class HomeLoan {
+  constructor(amount, duration) {
+    this.amount = amount;
+    this.duration = duration;
+    this.interest = 0.08;
+  }
+  calculateInterest() {
+    return this.amount * this.interest * this.duration;
+  }
+}
+class StudentLoan {
+  constructor(amount, duration) {
+    this.amount = amount;
+    this.duration = duration;
+    this.interest = 0.03;
+  }
+  calculateInterest() {
+    return this.amount * this.interest * this.duration;
+  }
+}
+class PersonalLoan {
+  constructor(amount, duration) {
+    this.amount = amount;
+    this.duration = duration;
+    this.interest = 0.05;
+  }
+  calculateInterest() {
+    return this.amount * this.interest * this.duration;
+  }
+}
+
+/** --------------------------- 用例测试 ------------------------------- **/
+var loan = new Loans();
+
+var homeLoan = loan.getloan('home', 3200, 5);
+console.log(homeLoan.calculateInterest()); // 1280
+
+var studentLoan = loan.getloan('student', 1800, 4);
+console.log(studentLoan.calculateInterest()); // 216
+
+var personalLoan = loan.getloan('personal', 1200, 2);
+console.log(personalLoan.calculateInterest()); // 120
