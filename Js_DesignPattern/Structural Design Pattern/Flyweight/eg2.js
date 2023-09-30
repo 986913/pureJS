6f0f72ff0a9d05c059ab2facda4b352ba8c181ef
Sun flower <MLIU2@LTU.EDU>
2023-09-30 16:11:29 -0400

    flyweight

--- Js_DesignPattern/Structural Design Pattern/Flyweight/eg2.js
+++ Js_DesignPattern/Structural Design Pattern/Flyweight/eg2.js
@@ -7,59 +7,38 @@ class Dress {
     this.availability = availability;
     this.price = 0;
   }
-  dressPrice() {}
+  dressPrice() {
+    if (this.type == 'maxi') this.price = 1000;
+    if (this.type == 'gown') this.price = 2000;
+    if (this.type == 'skirt') this.price = 500;
+    return this.price;
+  }
 }
 
-class Skirt extends Dress {
-  constructor(serialNumber, type, color, designer, availability) {
-    super(serialNumber, type, color, designer, availability);
-    this.price = 500;
-  }
-  dressPrice() {
-    return this.price;
-  }
-}
-class Gown extends Dress {
-  constructor(serialNumber, type, color, designer, availability) {
-    super(serialNumber, type, color, designer, availability);
-    this.price = 2000;
-  }
-  dressPrice() {
-    return this.price;
-  }
-}
-class Maxi extends Dress {
-  constructor(serialNumber, type, color, designer, availability) {
-    super(serialNumber, type, color, designer, availability);
-    this.price = 1000;
-  }
-  dressPrice() {
-    return this.price;
-  }
-}
 class DressFactory {
   constructor() {
-    this.dressMap = new Map();
+    this.existingDresses = {};
   }
+
   createDress(serialNumber, type, color, designer, availability) {
-    let dress = this.dressMap.get(type);
-    if (!dress) {
-      switch (type) {
-        case 'skirt':
-          dress = new Skirt(serialNumber, type, color, designer, availability);
-          break;
-        case 'gown':
-          dress = new Gown(serialNumber, type, color, designer, availability);
-          break;
-        case 'maxi':
-          dress = new Maxi(serialNumber, type, color, designer, availability);
-          break;
-      }
-      this.dressMap.set(type, dress);
+    const exists = this.existingDresses[serialNumber];
+
+    if (!!exists) {
+      return this.existingDresses[serialNumber];
+    } else {
+      const dress = new Dress(
+        serialNumber,
+        type,
+        color,
+        designer,
+        availability
+      );
+      this.existingDresses[serialNumber] = dress;
+      return dress;
     }
-    return dress;
   }
 }
+
 /** --------------------------- 用例测试 ------------------------------- **/
 const factory = new DressFactory();
 const pinkdress1 = factory.createDress('#123', 'skirt', 'pink', 'Zara', 'yes');
