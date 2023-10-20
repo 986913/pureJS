ff893da4d4f2e362e3fc5e5b51d16b6c4f86dc65
Sun flower <MLIU2@LTU.EDU>
2023-10-20 13:57:43 -0400

    change discripition

--- Js_Utilities/CompositionII.js
+++ Js_Utilities/CompositionII.js
@@ -5,19 +5,11 @@
  */
 
 /**
-  Here you are asked to create a pipe() function,  which chains multiple functions together to create a new function.
-  Suppose we have some simple functions like this:
-    const times = (y) =>  (x) => x * y
-    const plus = (y) => (x) => x + y
-    const subtract = (y) =>  (x) => x - y
-    const divide = (y) => (x) => x / y
-  Your pipe() would be used to generate new functions:
-    pipe([ times(2), times(3)])               // x * 2 * 3
-    pipe([ times(2), plus(3), times(4)])      // (x * 2 + 3) * 4
-    pipe([ times(2), subtract(3), divide(4)]) // (x * 2 - 3) / 4  
-  
-  notes: to make things simple, functions passed to pipe() will all accept 1 argument
-  
+  Implement a function **`compose`** that takes multiple functions as arguments and returns a new function that applies those functions in reverse order. 
+  The output of one function becomes the input of the next function, creating a chain of function compositions.
+  If there are no functions passed to the **`compose`** function, the default behavior is to return a new function that simply returns the input it receives. 
+  In other words, the default behavior of **`compose`** without any functions results in a simple identity function.
+  You may assume that all the functions accept a single parameter.
  */
 
 /* --------------------- 用例测试 ----------------------- */
