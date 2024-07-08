2ef8c8809e96d4595d957a06f88c2636f173f61b
Sun flower <MLIU2@LTU.EDU>
2024-07-08 18:35:39 -0400

    add new

--- Js_Utilities/手写JS的内置类或全局对象/Design LRU.lc146.js
+++ Js_Utilities/手写JS的内置类或全局对象/Design LRU.lc146.js
@@ -13,7 +13,7 @@ LRUCache.prototype.get = function (key) {
   if (!this.cache.has(key)) return -1;
 
   const value = this.cache.get(key);
-  this.cache.delete(key); // <--- 注意先delete
+  this.cache.delete(key); // <--- 注意先delete，目的是确保在使用一个新的键值对时，能够正确地维护LRU（Least Recently Used，最近最少使用）的顺序。
   this.cache.set(key, value);
   return value;
 };
@@ -23,7 +23,7 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-  this.cache.delete(key); // <--- 注意先delete
+  this.cache.delete(key); // <--- 注意先delete， 目的是确保在更新一个新的键值对时，能够正确地维护LRU（Least Recently Used，最近最少使用）的顺序。
   this.cache.set(key, value);
 
   if (this.cache.size > this.capacity) {
