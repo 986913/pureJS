6f3da183c96cbc3e68ed4f2686164a95d19aefd5
Sun flower <MLIU2@LTU.EDU>
2024-07-20 19:50:38 -0400

    2777

---
+++ Js_Utilities/Generator * + yield/Date Range Generator.lc2777.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {number} step
+ * @yields {string}
+ */
+
+var dateRangeGenerator = function* (start, end, step) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  while (startDate.getTime() <= endDate.getTime()) {
+    const date = String(startDate.getDate()).padStart(2, '0');
+    const month = String(startDate.getMonth() + 1).padStart(2, '0');
+    const year = String(startDate.getFullYear());
+
+    yield `${year}-${month}-${date}`;
+
+    const next = startDate.getDate() + step;
+    startDate.setDate(next); // <--- 主要在这， Date有个实例方法来设置日期.setDate()
+  }
+};
+
+/**
+ * const g = dateRangeGenerator('2023-04-01', '2023-04-04', 1);
+ * g.next().value; // '2023-04-01'
+ * g.next().value; // '2023-04-02'
+ * g.next().value; // '2023-04-03'
+ * g.next().value; // '2023-04-04'
+ * g.next().done; // true
+ */
