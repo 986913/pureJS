8d12462963cd07bac4f3b92c04327e4cf5061a8e
Sun flower <MLIU2@LTU.EDU>
2026-04-07 09:29:51 -0400

    update Observalble interval

--- Js_DesignPattern/Behavioral Design Pattern/Observer/implement_Observable_Interval.js
+++ Js_DesignPattern/Behavioral Design Pattern/Observer/implement_Observable_Interval.js
@@ -19,8 +19,15 @@ interval(1000).subscribe(console.log); //code prints 0, 1, 2 .... with an interv
 function interval(period) {
   return new Observable((sub) => {
     let i = 0;
-    setInterval(() => {
+    // 开始定时器，并将每次的 i 发送给订阅者
+    const timerId = setInterval((sub) => {
       sub.next(i++);
     }, period);
+
+    // 【加分项】：返回一个清理函数（Teardown）
+    // 当外部调用 unsubscribe() 取消订阅时，这个函数会自动执行，清除定时器防止内存泄漏
+    return () => {
+      clearInterval(timerId);
+    };
   });
 }
